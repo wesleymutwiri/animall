1d323cc36690f23935dfd8457f0a7dcb71c3e8e8
wesleymutwiri <wesleymochiemo@gmail.com>
2021-03-09 13:22:21 +0300

    added Timestamp and correct date format

--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
+	"strconv"
+	"time"
 
 	"github.com/gocolly/colly"
 )
 
 type show struct {
-	Title   string
-	Date    string
-	Episode string
-	// Time    string
+	Title       string
+	Date        string
+	Episode     string
+	Time        time.Time
+	CurrentTime time.Time
 }
 
 func main() {
@@ -32,21 +37,38 @@ func main() {
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
 	})
+
+	shows := make([]*show, 0)
+
 	c.OnHTML(".timetable .timetable-day", func(e *colly.HTMLElement) {
 		heading := e.ChildText(".timetable-day__heading h2")
-		fmt.Printf("Heading: %s \n", heading)
-		// time := e.ChildText(".timetable-timeslot .timetable-timeslot__content .timetable-timeslot__time .time")
-		e.ForEach(".timetable-timeslot .timetable-timeslot__content .timetable-anime-block", func(_ int, el *colly.HTMLElement) {
-			show := show{
-				Title:   el.ChildText(".body .title"),
-				Date:    heading,
-				Episode: el.ChildText(".timetable-anime-block .body .footer"),
-				// Time:    e.ChildText(".timetable-timeslot__time .time"),
+		// fmt.Printf("\n Heading: %s \n", heading)
+		// timeHeader := e.ChildText(".timetable-timeslot .timetable-timeslot__content .timetable-timeslot__time .time")
+		// fmt.Printf("\n %s \n", timeHeader)
+		e.ForEach(".timetable-timeslot .timetable-timeslot__content", func(_ int, el *colly.HTMLElement) {
+			show := &show{}
+			el.ForEach(".timetable-anime-block", func(_ int, another *colly.HTMLElement) {
+				show.Title = another.ChildText(".body .title")
+				show.Date = heading
+				show.Episode = another.ChildText(".timetable-anime-block .body .footer")
+				show.CurrentTime = time.Now()
+			})
+			scrappedTime, err := strconv.ParseInt(el.ChildAttr(".timetable-timeslot__time time", "data-timestamp"), 0, 64)
+			// fmt.Printf("Show Time: %d \n\n", scrappedTime)
+			if err != nil {
+				fmt.Printf("Error: %s", err)
 			}
-			fmt.Printf("Title: %s \n Date: %s \n Episode: %s \n \n", show.Title, show.Date, show.Episode)
+			show.Time = time.Unix(scrappedTime, 0)
+			// fmt.Printf("Show Time: %s \n\n", show.Time)
+			// fmt.Printf("Title: %s \n Date: %s \n Episode: %s \n Time: %s \n \n", show.Title, show.Date, show.Episode, show.Time)
+			shows = append(shows, show)
+			// fmt.Printf("Final List: %d", len(shows))
 		})
 	})
 	// Visit the site using the current URL
 	c.Visit("https://www.livechart.me/timetable")
 	// log.Printf("Finished scraping, check file %q for results \n", fName)
+	enc := json.NewEncoder(os.Stdout)
+	enc.SetIndent("", " ")
+	enc.Encode(shows)
 }
