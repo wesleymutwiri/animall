719ddf711d9baacaf0d36093c8c9d7d9011f42d4
wesleymutwiri <wesleymochiemo@gmail.com>
2021-04-01 00:32:17 +0300

    implemented sqlite database successfully and can save data
    Added new sqlite dependency as well

--- go.mod
+++ go.mod
@@ -8,18 +8,18 @@ require (
 	github.com/antchfx/htmlquery v1.2.3 // indirect
 	github.com/antchfx/xmlquery v1.3.5 // indirect
 	github.com/antchfx/xpath v1.1.11 // indirect
-	github.com/boltdb/bolt v1.3.1 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/boltdb/bolt v1.3.1
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gocolly/colly v1.2.0
 	github.com/golang/protobuf v1.4.3 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/mattn/go-sqlite3 v1.14.6 // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/spf13/cobra v1.1.3
 	github.com/stretchr/testify v1.4.0 // indirect
 	github.com/temoto/robotstxt v1.1.1 // indirect
 	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
 	golang.org/x/text v0.3.5 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
-	gopkg.in/yaml.v2 v2.2.5 // indirect
 )
--- schedules/scrap_schedule.go
+++ schedules/scrap_schedule.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"encoding/json"
+	"database/sql"
 	"fmt"
 	"log"
-	"os"
-	"strconv"
 	"time"
 
-	"github.com/boltdb/bolt"
-	"github.com/gocolly/colly"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type show struct {
@@ -21,52 +18,87 @@ type show struct {
 }
 
 func main() {
-	db, err := bolt.Open("../blog.db", 0600, nil)
+	db, err := sql.Open("sqlite3", "./blog.db")
+	checkErr(err)
+	_, err = db.Exec(`CREATE TABLE if not exists schedule(id integer,title text, date DATE, episode varchar, time datetime, currentTime datetime, PRIMARY KEY("id" AUTOINCREMENT)) `)
+	checkErr(err)
+	fmt.Println("DB created and opened successfully")
+	defer db.Close()
+	// c := colly.NewCollector()
+
+	// // threads := make(map[string][]show)
+
+	// c.OnRequest(func(r *colly.Request) {
+	// 	fmt.Println("Visiting", r.URL)
+	// })
+
+	// // shows := make([]*show, 0)
+
+	// c.OnHTML(".timetable .timetable-day", func(e *colly.HTMLElement) {
+	// 	heading := e.ChildText(".timetable-day__heading h2")
+	// 	// fmt.Printf("\n Heading: %s \n", heading)
+	// 	// timeHeader := e.ChildText(".timetable-timeslot .timetable-timeslot__content .timetable-timeslot__time .time")
+	// 	// fmt.Printf("\n %s \n", timeHeader)
+	// 	e.ForEach(".timetable-timeslot .timetable-timeslot__content", func(_ int, el *colly.HTMLElement) {
+	// 		show := show{}
+	// 		el.ForEach(".timetable-anime-block", func(_ int, another *colly.HTMLElement) {
+	// 			show.Title = another.ChildText(".body .title")
+	// 			show.Date = heading
+	// 			show.Episode = another.ChildText(".timetable-anime-block .body .footer")
+	// 			show.CurrentTime = time.Now()
+	// 		})
+	// 		scrappedTime, err := strconv.ParseInt(el.ChildAttr(".timetable-timeslot__time time", "data-timestamp"), 0, 64)
+	// 		// fmt.Printf("Show Time: %d \n\n", scrappedTime)
+	// 		if err != nil {
+	// 			fmt.Printf("Error: %s", err)
+	// 		}
+	// 		show.Time = time.Unix(scrappedTime, 0)
+	// 		// fmt.Printf("Show Time: %s \n\n", show.Time)
+	// 		// fmt.Printf("Title: %s \n Date: %s \n Episode: %s \n Time: %s \n \n", show.Title, show.Date, show.Episode, show.Time)
+	// 		addSchedule(db, show.Title, show.Date, show.Episode, show.Time, show.CurrentTime)
+	// 		// shows = append(shows, show)
+	// 		// fmt.Printf("Final List: %d", len(shows))
+	// 	})
+	// })
+	// // Visit the site using the current URL
+	// c.Visit("https://www.livechart.me/timetable")
+	// fmt.Println("Getting Schedule from db")
+	getSchedule(db)
+	// log.Printf("Finished scraping, check file %q for results \n", fName)
+	// GetAnimeList()
+	// enc := json.NewEncoder(os.Stdout)
+	// enc.SetIndent("", " ")
+	// enc.Encode(shows)
+}
+
+func checkErr(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("DB created and opened successfully")
-	defer db.Close()
-	c := colly.NewCollector()
+}
 
-	// threads := make(map[string][]show)
+func addSchedule(db *sql.DB, title string, date string, episode string, time time.Time, currentTime time.Time) {
+	tx, err := db.Begin()
+	checkErr(err)
+	stmt, _ := tx.Prepare("insert into schedule(title, date, episode, time, currentTime) values (?,?,?,?,?)")
+	checkErr(err)
+	_, err = stmt.Exec(title, date, episode, time, currentTime)
+	checkErr(err)
+	tx.Commit()
+}
 
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
-	})
-
-	shows := make([]*show, 0)
-
-	c.OnHTML(".timetable .timetable-day", func(e *colly.HTMLElement) {
-		heading := e.ChildText(".timetable-day__heading h2")
-		// fmt.Printf("\n Heading: %s \n", heading)
-		// timeHeader := e.ChildText(".timetable-timeslot .timetable-timeslot__content .timetable-timeslot__time .time")
-		// fmt.Printf("\n %s \n", timeHeader)
-		e.ForEach(".timetable-timeslot .timetable-timeslot__content", func(_ int, el *colly.HTMLElement) {
-			show := &show{}
-			el.ForEach(".timetable-anime-block", func(_ int, another *colly.HTMLElement) {
-				show.Title = another.ChildText(".body .title")
-				show.Date = heading
-				show.Episode = another.ChildText(".timetable-anime-block .body .footer")
-				show.CurrentTime = time.Now()
-			})
-			scrappedTime, err := strconv.ParseInt(el.ChildAttr(".timetable-timeslot__time time", "data-timestamp"), 0, 64)
-			// fmt.Printf("Show Time: %d \n\n", scrappedTime)
-			if err != nil {
-				fmt.Printf("Error: %s", err)
-			}
-			show.Time = time.Unix(scrappedTime, 0)
-			// fmt.Printf("Show Time: %s \n\n", show.Time)
-			// fmt.Printf("Title: %s \n Date: %s \n Episode: %s \n Time: %s \n \n", show.Title, show.Date, show.Episode, show.Time)
-			shows = append(shows, show)
-			// fmt.Printf("Final List: %d", len(shows))
-		})
-	})
-	// Visit the site using the current URL
-	c.Visit("https://www.livechart.me/timetable")
-	// log.Printf("Finished scraping, check file %q for results \n", fName)
-	// GetAnimeList()
-	enc := json.NewEncoder(os.Stdout)
-	enc.SetIndent("", " ")
-	enc.Encode(shows)
+func getSchedule(db *sql.DB) {
+	rows, err := db.Query("select * from schedule")
+	checkErr(err)
+	defer rows.Close()
+	for rows.Next() {
+		var id int
+		var title string
+		var date time.Time
+		var episode string
+		var time string
+		var currentTime string
+		rows.Scan(&id, &title, &date, &episode, &time, &currentTime)
+		fmt.Println("Schedule: ", title, " ", date, " ", episode, " ", time, " ", currentTime)
+	}
 }
