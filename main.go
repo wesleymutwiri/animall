b7d03288460a2a878a2926e69a7f18932c8c7822
wesleymutwiri <wesleymochiemo@gmail.com>
2021-04-01 12:44:21 +0300

    added anime list main file to get data from csv

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/csv"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type CsvLine struct {
@@ -14,6 +18,13 @@ type CsvLine struct {
 }
 
 func main() {
+	db, err := sql.Open("sqlite3", "blog.db")
+	checkErr(err)
+	_, err = db.Exec(`CREATE TABLE if not exists shows(id integer, name TEXT, episode TEXT, status TEXT, PRIMARY KEY("id" AUTOINCREMENT)) `)
+	checkErr(err)
+	fmt.Println("DB created and opened successfully")
+	defer db.Close()
+	GetAnimeList(db)
 	// fName := "latestanime.csv"
 	// file, err := os.create(fName)
 	// if err != nil {
@@ -25,7 +36,7 @@ func main() {
 
 	// writer.Write([]string{""})
 	// for reading the csv file provided
-	handleRequests()
+	// handleRequests()
 }
 
 func handleRequests() {
@@ -50,21 +61,53 @@ func ReadCsv(filename string) ([][]string, error) {
 	return lines, nil
 }
 
-func GetAnimeList() {
-	currentShows := make([]*CsvLine, 0)
+func GetAnimeList(db *sql.DB) {
+	// currentShows := make([]*CsvLine, 0)
 	lines, err := ReadCsv("AnimeList.csv")
 	if err != nil {
 		panic(err)
 	}
 
 	for _, line := range lines {
-		data := &CsvLine{
+		data := CsvLine{
 			Name:    line[0],
 			Episode: line[1],
 			Status:  line[3],
 		}
-		// fmt.Println(data.Name + " " + data.Episode + " " + data.Status)
-		currentShows = append(currentShows, data)
+		saveShows(db, data.Name, data.Episode, data.Status)
+		fmt.Println("Schedule saved", " ", data.Name, " ", data.Episode, " ", data.Status)
+		// currentShows = append(currentShows, data)
 		// fmt.Printf("Final List: %d", len(currentShows))
+	}
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func saveShows(db *sql.DB, name string, episode string, status string) {
+	tx, err := db.Begin()
+	checkErr(err)
+	stmt, _ := tx.Prepare("insert into shows(name, episode, status) values (?,?,?)")
+	checkErr(err)
+	_, err = stmt.Exec(name, episode, status)
+	checkErr(err)
+	fmt.Println("saved shows successfully")
+	tx.Commit()
+}
+
+func getAllShows(db *sql.DB) {
+	rows, err := db.Query("select * from schedule")
+	checkErr(err)
+	defer rows.Close()
+	for rows.Next() {
+		var id int
+		var name string
+		var episode string
+		var status string
+		rows.Scan(&id, &name, &episode, &status)
+		fmt.Println("Schedule: ", id, " ", name, " ", episode, " ", status)
 	}
 }
