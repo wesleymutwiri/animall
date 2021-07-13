0c9cd3318648bcba74677d7331fcac007a19842b
wesleymutwiri <wesleymochiemo@gmail.com>
2021-07-13 16:26:59 +0300

    successfully retrieved data from the google sheets API

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/wesleymutwiri/animall
 go 1.16
 
 require (
+	cloud.google.com/go v0.86.0 // indirect
 	github.com/PuerkitoBio/goquery v1.6.1 // indirect
 	github.com/andybalholm/cascadia v1.2.0 // indirect
 	github.com/antchfx/htmlquery v1.2.3 // indirect
@@ -11,15 +12,16 @@ require (
 	github.com/boltdb/bolt v1.3.1
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gocolly/colly v1.2.0
-	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/mattn/go-sqlite3 v1.14.6 // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/spf13/cobra v1.1.3
-	github.com/stretchr/testify v1.4.0 // indirect
 	github.com/temoto/robotstxt v1.1.1 // indirect
-	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
-	golang.org/x/text v0.3.5 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/oauth2 v0.0.0-20210628180205-a41e5a781914 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	google.golang.org/api v0.50.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.25.0 // indirect
+	google.golang.org/genproto v0.0.0-20210713002101-d411969a0d9a // indirect
 )
--- main.go
+++ main.go
@@ -1,70 +1,100 @@
 package main
 
 import (
-	"encoding/csv"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/option"
+	"google.golang.org/api/sheets/v4"
 )
 
-type CsvLine struct {
-	Name    string
-	Episode string
-	Status  string
+func getClient(config *oauth2.Config) *http.Client {
+	tokFile := "token.json"
+	tok, err := tokenFromFile(tokFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(tokFile, tok)
+	}
+	return config.Client(context.Background(), tok)
+}
+
+// Request a token from the web, then returns the retrieved token
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the authorization code: \n%v\n", authURL)
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code: %v", err)
+	}
+
+	tok, err := config.Exchange(context.TODO(), authCode)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from the web: %v", err)
+	}
+	return tok
+}
+
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+// save a token to a file path.
+func saveToken(path string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
 }
 
 func main() {
-	// fName := "latestanime.csv"
-	// file, err := os.create(fName)
-	// if err != nil {
-	// 	log.Fatalf("Cannot create file: %q: %s\n", fName, err)
-	// }
-	// defer file.Close()
-	// writer := csv.NewWriter(file)
-	// defer writer.Flush()
-
-	// writer.Write([]string{""})
-	// for reading the csv file provided
-	handleRequests()
-}
-
-func handleRequests() {
-	http.HandleFunc("/schedule", getLatestAnime)
-	log.Fatal(http.ListenAndServe(":10000", nil))
-}
-
-func getLatestAnime(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func ReadCsv(filename string) ([][]string, error) {
-	f, err := os.Open(filename)
+	ctx := context.Background()
+	b, err := ioutil.ReadFile("credentials.json")
 	if err != nil {
-		return [][]string{}, err
+		log.Fatalf("Unable to read client secret file: %v", err)
 	}
-	defer f.Close()
-	lines, err := csv.NewReader(f).ReadAll()
+	// if modifying these scopes, delete your previously saved token.json
+	config, err := google.ConfigFromJSON(b, "https://www.googleapis.com/auth/spreadsheets.readonly")
 	if err != nil {
-		return [][]string{}, err
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
 	}
-	return lines, nil
-}
-
-func GetAnimeList() {
-	currentShows := make([]*CsvLine, 0)
-	lines, err := ReadCsv("AnimeList.csv")
+	client := getClient(config)
+	srv, err := sheets.NewService(ctx, option.WithHTTPClient(client))
 	if err != nil {
-		panic(err)
+		log.Fatalf("Unable to retrieve Sheets client: %v", err)
+	}
+	spreadsheetId := "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms"
+	readRange := "Class Data!A2:E"
+	resp, err := srv.Spreadsheets.Values.Get(spreadsheetId, readRange).Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve data from sheet: %v", err)
 	}
 
-	for _, line := range lines {
-		data := &CsvLine{
-			Name:    line[0],
-			Episode: line[1],
-			Status:  line[3],
+	if len(resp.Values) == 0 {
+		fmt.Println("No data found.")
+	} else {
+		fmt.Println("Name, Major:")
+		for _, row := range resp.Values {
+			// Print columns A and E, which correspond to indices 0 and 4.
+			fmt.Printf("%s, %s\n", row[0], row[4])
 		}
-		// fmt.Println(data.Name + " " + data.Episode + " " + data.Status)
-		currentShows = append(currentShows, data)
-		// fmt.Printf("Final List: %d", len(currentShows))
 	}
+
 }
